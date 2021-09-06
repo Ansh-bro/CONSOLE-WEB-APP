d7ca50f8832d04179b702717a9bcf995052d47f2
Ansh-bro <87030975+Ansh-bro@users.noreply.github.com>
2021-09-06 22:22:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+<script>    
+var first-name="Shourya";
+var last-name="Jain"
+var full-name=first-name.concat(last-name);
+console.log(full-name);
+var a =10;
+var b =5;
+var addition = a+b ;
+console.log(addition);
+var length=full-name.length;
+console.log(length);
+</script>
